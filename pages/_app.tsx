e189d8da0193d08672d3fbd28eb4e60fb77a2329
oneraner <oneraner@gmail.com>
2021-06-21 12:59:57 +0800

    用 useReducer 模仿 Redux 更新State

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,50 @@
-import React, { useState,useContext, useReducer  } from 'react'
+import React, { useState, useContext, useReducer } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import  Header  from '../components/Header'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 interface ILogin {
-  isLogin:boolean,
-  email:string,
-  name:string,
-  setEmail:any,
-  setName:any,
-  setIsLogin:any
+    isLogin: boolean
+    email: string
+    name: string
 }
 const initialLogin = {
-  isLogin:false,
-  email:'',
-  name:'',
-  setEmail: () =>{},
-  setName: () =>{},
-  setIsLogin:() =>{}
+    isLogin: false,
+    email: '',
+    name: '',
 }
 
-export const AppContext = React.createContext<ILogin>(initialLogin);
+const loginReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'isLogin':
+            return { ...state, isLogin: action.data }
+            break
+        case 'email':
+            return { ...state, email: action.data }
+            break
+        case 'name':
+            return { ...state, name: action.data }
+            break
+        default:
+            return state
+    }
+}
+
+export const AppContext = React.createContext<{
+    loginState: ILogin
+    dispatch: React.Dispatch<any>
+}>({ loginState: initialLogin, dispatch: () => null })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLogin,setIsLogin] = useState(false)
-  const [email, setEmail] = useState('null')
-  const [name, setName] = useState('null')
+    const [loginState, dispatch] = useReducer(loginReducer, initialLogin)
 
-  return(
-  <AppContext.Provider value={{email, setEmail,name, setName,isLogin,setIsLogin}}>
-    <Header/>
-    <Component {...pageProps} />
-    <Footer />
-  </AppContext.Provider>)
+    return (
+        <AppContext.Provider value={{ loginState, dispatch }}>
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+        </AppContext.Provider>
+    )
 }
 export default MyApp
--- pages/register.tsx
+++ pages/register.tsx
@@ -4,71 +4,78 @@ import { GoogleLogin } from 'react-google-login'
 import { AppContext } from './_app'
 
 const refreshSetToken = async (res: any) => {
-  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
+    let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
 
-  const refreshToken = async () => {
-    const newAuthRes = await res.reloadAuthResponse()
-    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
-    console.log('newAuthRes', newAuthRes)
-    console.log('newAuthResToken', newAuthRes.id_token)
+    const refreshToken = async () => {
+        const newAuthRes = await res.reloadAuthResponse()
+        refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
+        console.log('newAuthRes', newAuthRes)
+        console.log('newAuthResToken', newAuthRes.id_token)
+        setTimeout(refreshToken, refreshTiming)
+    }
+
     setTimeout(refreshToken, refreshTiming)
-  }
-
-  setTimeout(refreshToken, refreshTiming)
 }
 
 const Register = () => {
-  const { setEmail, setName, isLogin, setIsLogin } = useContext(AppContext)
+    const { loginState, dispatch } = useContext(AppContext)
+    const isLogin = loginState.isLogin
 
-  const responseSuccess = (res: any) => {
-    setEmail(res.profileObj.email)
-    setName(res.profileObj.name)
-    setIsLogin(true)
-    router.push('content')
-    refreshSetToken(res)
-  }
-
-  const responseFailure = (res: any) => {
-    console.log('loginFailure', res)
-    setEmail('')
-    setName('')
-    setIsLogin(false)
-  }
-
-  useEffect(() => {
-    if (isLogin) {
-      router.push('/content')
+    const responseSuccess = (res: any) => {
+        dispatch({ type: 'name', data: res.profileObj.name })
+        dispatch({ type: 'email', data: res.profileObj.email })
+        dispatch({ type: 'isLogin', data: true })
+        router.push('content')
+        refreshSetToken(res)
     }
-  }, [isLogin])
-  return (
-    <>
-      <div className=" w-72 bg-background text-primary rounded-2xl p-6 my-6">
-        <p> E-mail</p>
-        <input className="w-full border-second border-2 p-2 mb-2" placeholder="請輸入E-mail" />
-        <p> Password</p>
-        <input className="w-full border-second border-2 p-2 mb-2" placeholder="請輸入密碼" />
-        <div className="flex justify-center mb-2 ">
-          <button
-            className=" w-6/12 h-9 bg-primary text-text rounded-md p-2"
-            onClick={() => {
-              alert('目前不提供註冊')
-            }}
-          >
-            註冊
-          </button>
-        </div>
-        <div className=" flex justify-center mb-2">
-          <GoogleLogin
-            clientId="848369820531-99mrj1ejd7i1qai3f9e0s0peuvkisbv7.apps.googleusercontent.com"
-            buttonText="使用 Google 帳號登入"
-            onSuccess={responseSuccess}
-            onFailure={responseFailure}
-            cookiePolicy={'single_host_origin'}
-          ></GoogleLogin>
-        </div>
-        <div className="text-center">Tips:只能用 Google 帳號登入</div>
-      </div>
-    </>
-  )
+
+    const responseFailure = (res: any) => {
+        console.log('loginFailure', res)
+        dispatch({ type: 'name', data: '' })
+        dispatch({ type: 'email', data: '' })
+        dispatch({ type: 'isLogin', data: false })
+    }
+
+    useEffect(() => {
+        if (isLogin) {
+            router.push('/content')
+        }
+    }, [isLogin])
+    return (
+        <>
+            <div className=" w-72 bg-background text-primary rounded-2xl p-6 my-6">
+                <p> E-mail</p>
+                <input
+                    className="w-full border-second border-2 p-2 mb-2"
+                    placeholder="請輸入E-mail"
+                />
+                <p> Password</p>
+                <input
+                    className="w-full border-second border-2 p-2 mb-2"
+                    placeholder="請輸入密碼"
+                />
+                <div className="flex justify-center mb-2 ">
+                    <button
+                        className=" w-6/12 h-9 bg-primary text-text rounded-md p-2"
+                        onClick={() => {
+                            alert('目前不提供註冊')
+                        }}
+                    >
+                        註冊
+                    </button>
+                </div>
+                <div className=" flex justify-center mb-2">
+                    <GoogleLogin
+                        clientId="848369820531-99mrj1ejd7i1qai3f9e0s0peuvkisbv7.apps.googleusercontent.com"
+                        buttonText="使用 Google 帳號登入"
+                        onSuccess={responseSuccess}
+                        onFailure={responseFailure}
+                        cookiePolicy={'single_host_origin'}
+                    ></GoogleLogin>
+                </div>
+                <div className="text-center">Tips:只能用 Google 帳號登入</div>
+            </div>
+        </>
+    )
 }
 export default Register
